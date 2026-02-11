4e7a17ac5cd63e6fed1f9032c21888bf002bc035
testu88 <testu88@users.noreply.github.com>
2026-02-11 17:21:53 +0800

    Edit +page.svelte

--- walking-skeleton/client/src/routes/+page.svelte
+++ walking-skeleton/client/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import {PUBLIC_API_URL} from "$env/static/public";
+    let todos = $state([]);
+
+    const fetchTodos = async () => {
+        const response = await fetch(`${PUBLIC_API_URL}/api/todos`);
+        const data = await response.json();
+        todos = data; 
+    };
+    $effect(()=> {
+        fetchTodos();
+    })
+</script>
+
+<h1>Todos</h1>
+
+<ul>
+    {#each todos as todo}
+    <li>{todo.name}</li>
+    {/each}
+</ul>
