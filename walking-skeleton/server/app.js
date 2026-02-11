31d9b0ec5b7d6c9f04f788feda4b6a53d1f54711
testu88 <testu88@users.noreply.github.com>
2026-02-11 16:38:40 +0800

    Import postgres

--- walking-skeleton/server/app.js
+++ walking-skeleton/server/app.js
@@ -1,8 +1,10 @@
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
+import postgres from "postgres";
 
 const app = new Hono();
+const sql = postgres();
 
 app.use("/*", cors());
 app.use("/*", logger());
@@ -11,6 +13,12 @@ let visits = 0;
 app.get("/api/visits", (c) => {
     visits++;
     return c.json({visits});
+});
+
+// retrieve todos from postgres
+app.get("/api/todos", async (c) => {
+    const todos = await sql `SELECT * FROM todos`;
+    return c.json(todos);
 });
 
 export default app;
