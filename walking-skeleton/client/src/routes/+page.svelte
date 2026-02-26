a2fe85448e3e9581921cb48dd08e04e3ae364647
testu88 <testu88@users.noreply.github.com>
2026-02-26 16:49:16 +0800

    Add events and methods

--- walking-skeleton/client/src/routes/+page.svelte
+++ walking-skeleton/client/src/routes/+page.svelte
@@ -5,6 +5,28 @@
 
 
     let todos = $state([]);
+    let text = $state("");
+    let isChecked = $state(false);
+    let countState = $state({
+        count: 0,
+        note: "single digit",
+        numbers: [],
+    });
+
+    /*const textChanged = (event) => {
+        text = event.target.value;
+    }*/
+
+    const increment = () => {
+        countState.count++;
+
+        countState.numbers.push(countState.count);
+
+        if (countState.count > 9) {
+            countState.note = "not single digit";
+        }
+    };
+
 
     const fetchTodos = async () => {
         const response = await fetch(`${PUBLIC_API_URL}/api/todos`);
@@ -18,6 +40,22 @@
 
 
 <Header />
+
+<p>Text : {text}</p>
+<input type="text" bind:value={text} />
+
+<p>Checkbox is: {isChecked ? "checked": "not checked"} </p>
+<input type="checkbox" bind:checked={isChecked} />
+
+<p>Count: {countState.count}, Note: {countState.note}</p>
+<button onclick={increment}>Increment</button>
+{#if countState.numbers === 10}
+<p>You have {countState.count}</p>
+{:else if countState.numbers < 9}
+<p>You have less than 10.</p>
+{:else}
+<p>You have more than 10.</p>
+{/if}
 
 
 <h1>Todos</h1>
